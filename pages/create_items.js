9c518e1a9e2a75a1a58da23773914d82988202e5
nirban256 <nirban256@gmail.com>
2022-05-17 13:33:44 +0530

    deployed contract to local node

---
+++ pages/create_items.js
@@ -0,0 +1,13 @@
+import { ethers } from "ethers";
+import Web3modal from "web3modal";
+import React, { useState } from "react";
+import { create as ipfsHttpClient } from "ipfs-http-client";
+import { useRouter } from "next/router";
+
+const create_items = () => {
+    return (
+        <div>create_items</div>
+    )
+}
+
+export default create_items;
--- pages/index.js
+++ pages/index.js
@@ -67,7 +67,21 @@ export default function Home() {
           {
             nfts.map((nft, i) => {
               <div key={i} className="border shadow rounded-xl overflow-hidden">
-
+                <img src={nft.image} alt="nfts" />
+                <div className="p-4">
+                  <p style={{ height: "64px" }} className="text-2xl font-semibold">
+                    {nft.name}
+                  </p>
+                  <div style={{ height: '70px' }} className="overflow-hidden">
+                    <p className="text-gray-400">
+                      {nft.description}
+                    </p>
+                  </div>
+                </div>
+                <div className="p-4 bg-black">
+                  <p className="text-2xl mb-4 font-bold text-white">(nft.price) Matic</p>
+                  <button className="w-full bg-pink-500 text-white font-bold py-2 px-12 rounded" onClick={() => buyNFTs(nft)}>Buy</button>
+                </div>
               </div>
             })
           }
